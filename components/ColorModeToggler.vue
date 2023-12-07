1d9b2a05197ad12abb7ad1558e2f96d3dd174d01
Artur Dorfman <a.dorfman@softonix.org>
2023-12-07 18:29:28 +0200

    added color mode toggler

---
+++ components/ColorModeToggler.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="toggleMode">{{ nextMode }}</button>
+</template>
+
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+const modes = ['system', 'light', 'dark']
+
+const nextMode = computed(() => {
+  const currentIndex = modes.indexOf(colorMode.preference)
+  return currentIndex + 1 === modes.length
+    ? modes[0]
+    : modes[currentIndex + 1]
+})
+
+const toggleMode = () => colorMode.preference = nextMode.value
+</script>
