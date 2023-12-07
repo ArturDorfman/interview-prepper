f02471208fe3a2cf1b16e3a71705c09be9dcb65d
Artur Dorfman <a.dorfman@softonix.org>
2023-12-07 18:28:07 +0200

    added meta preferences

--- app.vue
+++ app.vue
@@ -1,4 +1,35 @@
 <template>
-  <div>
+  <div class="container mx-auto">
+    <Header />
   </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: '[title]',
+  description: '[description]',
+  // ogTitle: '[og:title]',
+  // ogDescription: '[og:description]',
+  // ogImage: '[og:image]',
+  // ogUrl: '[og:url]'
+})
+
+useHead({
+  titleTemplate:'Interview prepper - %s',
+  link: [
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com'
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.gstatic.com'
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=DM+Sans:opsz@9..40&display=swap',
+      crossorigin: ''
+    }
+  ]
+})
+</script>
