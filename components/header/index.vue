744600e961b3981f56c017246a1b49569641003d
Artur Dorfman <a.dorfman@softonix.org>
2023-12-08 12:00:57 +0200

    refactored header

--- components/header/index.vue
+++ components/header/index.vue
@@ -1,8 +1,18 @@
-
 <template>
-  <header class="flex justify-between">
-    <HeaderLogo />
-    <HeaderMenu />
-    <ColorModeToggler />
+  <header class="bg-background/75 backdrop-blur border-b border-gray-200 dark:border-gray-800 -mb-px sticky top-0 z-50 lg:mb-0 lg:border-0">
+    <div class="flex justify-between items-center h-[--header-height]">
+      <HeaderLogo />
+      <HeaderMenu />
+  
+      <ClientOnly>
+        <ColorModeToggler />
+      </ClientOnly>
+    </div>
   </header>
 </template>
+
+<style>
+header {
+  --header-height: 64px;
+}
+</style>
