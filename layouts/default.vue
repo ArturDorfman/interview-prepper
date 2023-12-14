bb6c6cdf4e3690bf60d0e382255c800f461cf749
Artur Dorfman <a.dorfman@softonix.org>
2023-12-14 11:34:24 +0200

    removed side bar navigation from default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,5 @@
 <template>
-  <main class="max-w-7xl mx-auto w-full flex gap-8">
-    <div class="basis-1/4">
-      <SideBarNavigation />
-    </div>
-
+  <main class="max-w-7xl mx-auto w-full">
     <slot />
   </main>
 </template>
