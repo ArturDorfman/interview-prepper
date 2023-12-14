d8e2a88cfffaf55c9c53d3adbc566ed772f1f46a
Artur Dorfman <a.dorfman@softonix.org>
2023-12-14 14:31:00 +0200

    added tailwind vars to main.css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,19 @@
-module.exports = {
+import type { Config } from 'tailwindcss'
+import tailwindTypography from '@tailwindcss/typography'
+
+export default <Partial<Config>> {
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-text)',
+        title: 'var(--color-title)',
+      },
+      fontFamily: {
+        sans: ['DM Sans']
+      }
+    }
+  },
   darkMode: 'class',
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [tailwindTypography],
 }
