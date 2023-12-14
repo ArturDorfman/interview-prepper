56e4ae4f3c39578861bdb0c68f542ac27a7e4090
Artur Dorfman <a.dorfman@softonix.org>
2023-12-14 17:08:11 +0200

    highlighted active nav item

--- components/SideBarNavigation.vue
+++ components/SideBarNavigation.vue
@@ -1,7 +1,11 @@
 <template>
   <section class="sticky top-16">
     <ul>
-      <li v-for="navItem in navList" :key="navItem._path">
+      <li
+        v-for="navItem in navList"
+        :key="navItem._path"
+        :class="{ 'text-primary': navItem._path === route.path }"
+      >
         <NuxtLink :to="navItem._path">{{ navItem.title }}</NuxtLink>
       </li>
     </ul>
@@ -9,6 +13,7 @@
 </template>
 
 <script setup lang="ts">
+const route = useRoute()
 const { data: navigation } = await useAsyncData('navigation', () => fetchContentNavigation())
 const navList = computed(() => (navigation.value || [])[0].children)
 </script>
