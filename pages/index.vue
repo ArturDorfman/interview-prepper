944a559e5e1ecf0ca6de225c0d0364c43dc29876
Artur Dorfman <a.dorfman@softonix.org>
2023-12-14 14:30:33 +0200

    refactored styles on home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="flex flex-col items-center justify-center">
     <h1 class="text-7xl">
-      The <span class="text-green-500 dark:text-green-400">Interview</span> Preparation
+      The <span class="text-primary dark:text-primary">Interview</span> Preparation
     </h1>
 
     <h4 class="mt-6 text-lg dark:text-gray-400">
@@ -12,7 +12,7 @@
       <NuxtLink
           to="/guide/markup"
           class="rounded-md px-3.5 py-2.5 text-white dark:text-gray-900
-        bg-green-500 hover:bg-green-600 dark:bg-green-400 dark:hover:bg-green-500
+          bg-primary hover:bg-green-600 dark:bg-primary dark:hover:bg-primary
           flex justify-center items-center"
       >
         <Icon class="h-6 w-6 mr-1.5" name="i-ph-rocket-launch-duotone" />
