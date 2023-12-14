bf9e7a48da0ea4f0c866035af4ef1b33fe1c9ed4
Artur Dorfman <a.dorfman@softonix.org>
2023-12-14 11:33:37 +0200

    replaced flex to grid

--- pages/guide/[...slug].vue
+++ pages/guide/[...slug].vue
@@ -3,12 +3,16 @@
     dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300"
   >
     <ContentDoc v-slot="{ doc }">
-      <div class="flex justify-between gap-8">
-        <div class="w-full">
+      <div class="grid grid-cols-12 gap-8">
+        <div class="not-prose col-span-2">
+          <SideBarNavigation />
+        </div>
+
+        <div class="col-span-8">
           <ContentRenderer :value="doc" />
         </div>
 
-        <div class="not-prose flex justify-end">
+        <div class="not-prose col-span-2">
           <TableOfContents :links="doc.body.toc.links" />
         </div>
       </div>
