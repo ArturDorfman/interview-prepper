955da7e383719cf2ef020457a55aa30030e47d44
Artur Dorfman <a.dorfman@softonix.org>
2023-12-18 20:49:06 +0200

    changed sidebar navigation

--- components/SideBarNavigation.vue
+++ components/SideBarNavigation.vue
@@ -1,17 +1,28 @@
 <template>
   <section class="sticky top-16">
     <ul>
-      <li v-for="navItem in navList" :key="navItem._path">
-        <NuxtLink
-          :to="navItem._path"
-          class="text-gray-500 dark:text-gray-400 duration-200"
-          :class="{
-            'hover:text-black dark:hover:text-white': navItem._path !== route.path,
-            'text-primary dark:text-primary': navItem._path === route.path
-          }"
-        >
-          {{ navItem.title }}
-        </NuxtLink>
+      <li
+        v-for="([groupName, groupedContent]) in contentGroups"
+        :key="groupName"
+      >
+        <ul class="mb-3">
+          <p class="uppercase cursor-default">{{ groupName }}</p>
+          <li
+            v-for="contentItem in groupedContent"
+            :key="contentItem._path"
+          >
+            <NuxtLink
+                :to="contentItem._path"
+                class="block ml-3 text-gray-500 dark:text-gray-400 duration-200"
+                :class="{
+              'hover:text-black dark:hover:text-white': contentItem._path !== route.path,
+              'text-primary dark:text-primary': contentItem._path === route.path
+            }"
+            >
+              {{ contentItem.title }}
+            </NuxtLink>
+          </li>
+        </ul>
       </li>
     </ul>
   </section>
@@ -19,6 +30,21 @@
 
 <script setup lang="ts">
 const route = useRoute()
-const { data: navigation } = await useAsyncData('navigation', () => fetchContentNavigation())
-const navList = computed(() => (navigation.value || [])[0].children)
+const { data: contentList } = await useAsyncData('nav', () => queryContent('/guide').find())
+
+const contentGroups = computed(() => {
+  const contentByGroupId = (contentList.value ?? []).reduce((contentGroups, { groupId, _path, _id, title }) => {
+    const key = groupId
+    const value = { _path, title }
+    const collection = contentGroups.get(key);
+    if (!collection) {
+      contentGroups.set(key, [value])
+    } else {
+      collection.push(value)
+    }
+    return contentGroups
+  }, new Map)
+
+  return new Map([...contentByGroupId].sort())
+})
 </script>
