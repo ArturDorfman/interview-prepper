bd7fc1850be8b04af467e265c158c1404cb9e548
Artur_Dorfman <artur.dorfman@gmail.com>
2024-03-22 16:42:11 +0200

    Update button hover state color in dark mode.

--- pages/index.vue
+++ pages/index.vue
@@ -10,10 +10,10 @@
 
     <button class="mt-10 z-10">
       <NuxtLink
-          to="/guide/interview-questions"
-          class="rounded-md px-3.5 py-2.5 text-white dark:text-gray-900
-          bg-primary hover:bg-green-600 dark:bg-primary dark:hover:bg-primary
-          flex justify-center items-center"
+        to="/guide/interview-questions"
+        class="rounded-md px-3.5 py-2.5 text-white dark:text-gray-900
+        bg-primary hover:bg-green-600 dark:bg-primary dark:hover:bg-gray-100
+        flex justify-center items-center"
       >
         <Icon class="h-6 w-6 mr-1.5" name="i-ph-rocket-launch-duotone" />
         Get started
